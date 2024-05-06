478f6be16cf7936bd5cf209ca81c12452c542a0b
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-06 11:18:39 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result
+  if( number < 400 ) {
+    result = 'This one is on me!';
+  } else if (number > 400 && number < 2000 ) {
+    result = 'That will be twenty bucks.';
+  } else if (number >= 2001 && number < 2500 ) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ const message = city === 'NYC' ? "Ok, sounds good." : "No go.";
+ return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+      return 'Bye.'
+  }
 }
